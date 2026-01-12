c1660de005a10e3104218d9f4fdbf9f01975c282
Zhihao Wang <wangzhihao9@hotmail.com>
2026-01-12 11:17:16 +0800

    remove test scripts.

--- llvm/sodium_coremark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-model=pic
-#debug=-DCORE_DEBUG=1
-debug=""
-./bin/clang $debug -mllvm -sodium-pair-spillrestore --target=sodium16 -fPIC -O$1 -c ~/sodium/coremark/*.c
-./bin/clang --target=sodium16 -fPIC -c ~/crt.S
-./bin/ld.lld -T ../link.ld crt.o core_list_join.o core_util.o core_main.o core_matrix.o core_portme.o core_state.o libc.o divsi.o divhi.o ee_printf.o
-./bin/llvm-objdump -d a.out &> result.S
-./bin/llvm-objcopy a.out -O binary ram.bin
--- llvm/sodium_gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-model=pic
-./bin/clang --target=sodium16 -fPIC -c ~/crt.S
-./bin/clang --target=sodium16 -fPIC -O$1 -c ~/main.c
-./bin/ld.lld -T ../link.ld crt.o main.o
-./bin/llvm-objdump -d a.out &> result.S
-./bin/llvm-objcopy a.out -O binary ram.bin
